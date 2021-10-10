108215ed57d24f96c1abca88efb1ceca3bc69e3b
David Prenninger <david.prenninger@gmx.de>
2021-10-10 11:05:03 +0200

    [API] Fix API location

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
-const webSocketURL = process.env.NODE_ENV === 'production' ? 'wss://api.kingscastle.neuz8t.de' : 'ws://localhost:3031';
+const webSocketURL = process.env.NODE_ENV === 'production' ? 'wss://api.kingscastle.io' : 'ws://localhost:3031';
 Vue.use(VueNativeSock, webSocketURL, { reconnection: true, reconnectionDelay: 100, format: 'json' });
 
 new Vue({
